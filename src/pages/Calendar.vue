c45444796dfc54e85575200674275217196813cb
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2021-10-05 10:29:57 +0600

    Initial commit

---
+++ src/pages/Calendar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+    <Month class="col-4"/>
+  </div>
+</template>
+<script>
+import Month from "@/components/Month";
+export default {
+  name: "Calendar",
+  components: {Month}
+}
+</script>
+
+<style scoped>
+
+</style>
